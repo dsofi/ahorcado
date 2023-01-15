000f5eabb346db202c9bbcd93500a05c0070ca7a
Sofi <dornasofia@gmail.com>
2023-01-15 00:34:25 -0300

    Primer commit. Funcionando. Basico

---
+++ palabras.js
@@ -0,0 +1,5 @@
+const palabras = [
+    'sol', 'encendedor', 'termo', 'mate', 'yerba', 'azucar', 'televisor', 'pantalla', 'vaso', 'luna',
+    'carne', 'martillo', 'lavadora', 'cangrejo', 'computadora', 'ventilador', 'canilla', 'mesa', 'luz', 
+    'mochila', 'empanada', 'pollo', 'habitacion', 'diccionario', 'enciclopedia', 'ventana', 'edificio'
+]
---
+++ script.js
@@ -0,0 +1,114 @@
+const contPalabra = document.getElementById("contPalabra");
+const startButton = document.getElementById("startButton");
+const letrasUsadasElement = document.getElementById("letrasUsadas");
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.canvas.width = 0;
+ctx.canvas.height = 0;
+
+const partesCuerpo = [
+    [4,2,1,1],
+    [4,3,1,2],
+    [3,5,1,1],
+    [5,5,1,1],
+    [3,3,1,1],
+    [5,3,1,1]
+]
+
+let palabraElegida;
+let letrasUsadas;
+let errores;
+let aciertos;
+
+const agregarLetra = letra => {
+    const letraElement = document.createElement("span");
+    letraElement.innerHTML = letra;
+    letrasUsadasElement.appendChild(letraElement);
+}
+
+const agregarCuerpo = partesCuerpo => {
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(...partesCuerpo);
+}
+
+const letraIncorrecta = () => {
+    agregarCuerpo(partesCuerpo[errores]); 
+    errores++;
+    if(errores === partesCuerpo.length) fin();   
+}
+
+const fin = () => {
+    document.removeEventListener("keydown", eventoLetra);
+    startButton.style.display = "block";
+}
+
+const letraCorrecta = letra => {
+    const { children } = contPalabra;
+    for(let i=0; i<children.length; i++){
+        if(children[i].innerHTML === letra){
+            children[i].classList.toggle("hidden");
+            aciertos++;
+        }
+    }
+    if(aciertos === palabraElegida.length) fin();
+}
+
+const letraIngresada = letra => {
+    if(palabraElegida.includes(letra)){
+        letraCorrecta(letra);
+    } else{
+        letraIncorrecta();
+    }
+    agregarLetra(letra);
+    letrasUsadas.push(letra);
+}
+
+const eventoLetra = event => {
+    let nuevaLetra = event.key.toUpperCase();
+    if(nuevaLetra.match(/^[a-zñ]$/i) && !letrasUsadas.includes(nuevaLetra)){
+        letraIngresada(nuevaLetra);
+    }
+}
+
+const dibujarPalabra = () => {
+    palabraElegida.forEach(letra => {
+        const letraElement = document.createElement("span");
+        letraElement.innerHTML = letra;
+        letraElement.classList.add("letra");
+        letraElement.classList.add("hidden");
+        contPalabra.appendChild(letraElement);
+    })
+}
+
+const selectPalabraAleatoria = () => {
+    let palabra = palabras[Math.floor((Math.random() * palabras.length))].toUpperCase();
+    palabraElegida = palabra.split('');
+}
+
+const dibujarEstructura = () =>{
+    ctx.canvas.width = 120;
+    ctx.canvas.height = 160;
+    ctx.scale(20,20);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#d95d39";
+    ctx.fillRect(0, 7, 4, 1);
+    ctx.fillRect(1, 0, 1, 8);
+    ctx.fillRect(2, 0, 3, 1);
+    ctx.fillRect(4, 1, 1, 1);
+}
+
+const startGame = () =>{
+    letrasUsadas = [];
+    errores = 0;
+    aciertos = 0;
+    contPalabra.innerHTML = "";
+    letrasUsadasElement.innerHTML = "";
+    startButton.style.display = "none";
+    dibujarEstructura();
+    selectPalabraAleatoria();
+    dibujarPalabra();
+    document.addEventListener("keydown", eventoLetra);
+}
+
+startButton.addEventListener("click", startGame);
